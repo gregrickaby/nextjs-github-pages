c6637c21e5af018ea9f2bf140ba5d4ff61798a8a
Greg Rickaby <gregrickaby@gmail.com>
2021-07-07 21:43:03 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 // Replace `'nextjs-github-pages'` with your Github repo project name.
 module.exports = {
-  assetPrefix: !debug ? "/nextjs-github-pages/" : "",
+  assetPrefix: "/nextjs-github-pages/",
 };
