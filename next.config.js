3cb05abafb0ec074d8c0800855b66d200b70f816
Greg Rickaby <gregrickaby@gmail.com>
2021-07-07 21:56:46 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-// Replace `'nextjs-github-pages'` with your Github repo project name.
+// Replace `nextjs-github-pages` with your Github repo project name.
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  assetPrefix: "/nextjs-github-pages/",
-};
+  // Use the prefix in production and not development.
+  assetPrefix: isProd ? '/nextjs-github-pages/' : '',
+}
