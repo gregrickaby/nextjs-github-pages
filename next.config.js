81e66f522f9f26d3ae8da76bfdb499619033bcac
Greg Rickaby <greg.rickaby@americaneagle.com>
2022-09-09 10:02:31 -0500

    update to next 12.3

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
@@ -6,4 +7,12 @@ module.exports = {
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
   assetPrefix: isProd ? "/nextjs-github-pages/" : "",
+  /**
+   * Disable server-based image optimization.
+   *
+   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
+   */
+  images: {
+    unoptimized: true,
+  },
 };
