70bcdcff1e45a28bd10df2ac99a69ae0a9dad33a
Jack Zhang <zhiyi.zhang.duke@gmail.com>
2023-02-11 21:39:59 -0500

    Initial commit

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
    * Tell Next.js where the `public` folder is.
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
-  assetPrefix: isProd ? "/nextjs-github-pages/" : "",
+  assetPrefix: isProd ? "/jack-blog/" : "",
   /**
    * Disable server-based image optimization.
    *
