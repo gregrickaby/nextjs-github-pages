b8fbb82db6afd70e5c15a09697e20ae11787b558
Greg Rickaby <gregrickaby@gmail.com>
2023-06-19 09:19:17 -0500

    update comments

--- next.config.js
+++ next.config.js
@@ -5,21 +5,22 @@
  **/
 const nextConfig = {
   /**
-   * Enable static exports for App Router.
+   * Enable static exports for the App Router.
    *
    * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
    */
   output: "export",
 
   /**
-   * Set base path for GitHub Pages.
+   * Set base path. This is usually the name of your repository.
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
   basePath: "/nextjs-github-pages",
 
   /**
-   * Disable server-based image optimization.
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
    *
    * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
    */
