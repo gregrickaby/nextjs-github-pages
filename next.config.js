f16eda616ba1a78a86a313b4b174d3eb8aedc9dd
Greg Rickaby <gregrickaby@gmail.com>
2023-06-19 08:31:35 -0500

    use official gh action and new gh pages workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 // @ts-check
 
-const isProd = process.env.NODE_ENV === "production";
-
 /**
  * @type {import('next').NextConfig}
  **/
@@ -12,23 +10,6 @@ const nextConfig = {
    * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
    */
   output: "export",
-
-  /**
-   * Tell Next.js where the `public` folder is.
-   * Replace `nextjs-github-pages` with your Github repo project name.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/assetPrefix
-   */
-  assetPrefix: isProd ? "/nextjs-github-pages/" : "",
-
-  /**
-   * Disable server-based image optimization.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
