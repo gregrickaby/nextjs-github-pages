b20c21e9159c880a45d4101eb6a2dd3d2e7465b7
danji90 <danji_ma90@hotmail.com>
2025-10-15 15:03:23 +0200

    fix(lint): replace eslint with biome

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,28 +1,25 @@
-import type {FC} from 'react';
-
-interface IProps {}
+import type { FC } from "react";
 
 /**
-* @author
-* @function @Header
-**/
+ * @author
+ * @function @Header
+ **/
 
-export const Header:FC<IProps> = (props) => {
+export const Header: FC = () => {
   return (
     <header className="sticky top-0">
-        <nav>
-            <ul className="px-10 py-5 flex gap-10 justify-end">
-                <li>About</li>
-                <li>Projects</li>
+      <nav>
+        <ul className="px-10 py-5 flex gap-10 justify-end">
+          <li>About</li>
+          <li>Projects</li>
 
-                
-                <li>Experience</li>
-                <li>Github</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
+          <li>Experience</li>
+          <li>Github</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
     </header>
-   )
- }
+  );
+};
 
- export default Header;
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        
-      </footer>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
