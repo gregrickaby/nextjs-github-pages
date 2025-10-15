6ac212816a6bf197ac0977b16c4edd0a5dcc5888
danji90 <danji_ma90@hotmail.com>
2025-10-15 12:16:44 +0200

    chore: add Header component

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import type {FC} from 'react';
+
+interface IProps {}
+
+/**
+* @author
+* @function @Header
+**/
+
+export const Header:FC<IProps> = (props) => {
+  return (
+    <header className="sticky top-0">
+        <nav>
+            <ul className="px-10 py-5 flex gap-10 justify-end">
+                <li>About</li>
+                <li>Projects</li>
+
+                
+                <li>Experience</li>
+                <li>Github</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </header>
+   )
+ }
+
+ export default Header;
